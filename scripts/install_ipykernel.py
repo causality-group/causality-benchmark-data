e2c60b9aead453cb90df1ec48223e3d8b922bf6c
vcausa <146931608+vcausa@users.noreply.github.com>
2024-02-20 17:22:03 +0100

    Fix install ipykernel script and add uninstall script

--- scripts/install_ipykernel.py
+++ scripts/install_ipykernel.py
@@ -1,11 +1,20 @@
 """Script to install causality ipykernel."""
 
+import logging
 from ipykernel.kernelspec import install
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 
 def install_ipykernel():
     """Installs causality ipykernel."""
-    install(user=True, kernel_name="cauality-forecast-darts-p3.9")
+    try:
+        install(user=True, kernel_name="causality-forecast-darts-p3.9")
+        logging.info("Successfully installed causality ipykernel.")
+    except Exception as e:
+        logging.error("Failed to install causality ipykernel.")
+        logging.error(repr(e))
 
 
 if __name__ == "__main__":
