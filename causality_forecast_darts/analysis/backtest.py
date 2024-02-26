b5718df9b0560c06fe022cb71318f41e057b0673
Mark Horvath <mark.horvath@causality-group.com>
2024-02-26 14:01:25 -0500

    Optionally return signal in backtest_df()

--- causality_forecast_darts/analysis/backtest.py
+++ causality_forecast_darts/analysis/backtest.py
@@ -1,6 +1,6 @@
 
 
-def signalbacktest_df(signal_df, upcoming_ret_df, enter_at_stdev=2.0):
+def signalbacktest_df(signal_df, upcoming_ret_df, enter_at_stdev=2.0, do_return_signal=False):
     """A simple backtest, trading cross-sectional signals at a given standard deviation.
 
     Invests $0.5 both long and short, hence results can be interpreted as strategy returns.
@@ -9,6 +9,7 @@ def signalbacktest_df(signal_df, upcoming_ret_df, enter_at_stdev=2.0):
         signal_df: DataFrame with signals observable at the Timestamps in in its index.
         upcoming_ret_df: DataFrame with forward looking returns tradeable at the Timestamps in in its index.
         enter_at_stdev: Number of standard deviations to enter the trade both on long and short side.
+        do_return_signal: If True, returns the signal DataFrame as well, otherwise only the strategy returns.
     
     Returns:
         DataFrame with strategy returns.
@@ -22,4 +23,7 @@ def signalbacktest_df(signal_df, upcoming_ret_df, enter_at_stdev=2.0):
     # Normalize cross-sectionally:
     signal_df = signal_df.div(signal_df.abs().sum(axis=1), axis=0)
 
-    return signal_df * upcoming_ret_df
+    if do_return_signal:
+        return signal_df * upcoming_ret_df, signal_df
+    else:
+        return signal_df * upcoming_ret_df
